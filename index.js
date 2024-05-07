22e314040f146596602380b348a9d39b077e5b9b
Jessie Grosen <jmgrosen@gmail.com>
2024-05-07 17:36:45 -0400

    start to add the tape-to-guide-to-head process

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 import { Application, Graphics, GraphicsPath } from './pixi.mjs';
 
+const PARAMS = {
+  reelOnePos: [150, 150],
+  reelTwoPos: [375, 150],
+  reelInnerRadius: 35,
+  reelOuterRadius: 100,
+  tapeThickness: 0.01,
+  tapeTotalLength: 2000000,
+  tapeSpeed: 25,
+  guideOnePos: [250, 275]
+};
+
 const reel = () => {
   const arcSize = Math.PI / 6;
   const sixth = Math.PI / 6;
@@ -14,11 +25,11 @@ const reel = () => {
     .arc(0, 0, 75, 9 * sixth - arcSize/2, 9 * sixth + arcSize/2)
     .closePath();
   return new Graphics()
-    .circle(0, 0, 100)
+    .circle(0, 0, PARAMS.reelOuterRadius)
     .fill('grey')
     .path(cutPath)
     .cut()
-    .circle(0, 0, 35)
+    .circle(0, 0, PARAMS.reelInnerRadius)
     .fill('grey');
 };
 
@@ -26,7 +37,7 @@ const innerRadius = 35;
 const tapeThickness = 0.01;
 
 const tapeRadius = (tapeLength) => {
-  return Math.sqrt(innerRadius * innerRadius + tapeThickness * tapeLength / Math.PI);
+  return Math.sqrt(Math.pow(PARAMS.reelInnerRadius, 2) + tapeThickness * tapeLength / Math.PI);
 };
 
 const tape = (g, length) => {
@@ -35,37 +46,57 @@ const tape = (g, length) => {
     .fill('black');
 };
 
+const tangentPoint = ([circleX, circleY], r, [pointX, pointY]) => {
+  const x = pointX - circleX;
+  const y = pointY - circleY;
+  const d = Math.sqrt(x*x + y*y);
+  return [
+    circleX + (r*r / (d*d)) * x + r / (d*d) * Math.sqrt(d*d - r*r) * (-y),
+    circleY + (r*r / (d*d)) * y + r / (d*d) * Math.sqrt(d*d - r*r) * x
+  ];
+};
+
+const runningTape = (length) => {
+  const [x, y] = tangentPoint(PARAMS.reelOnePos, tapeRadius(length), PARAMS.guideOnePos);
+  return new GraphicsPath()
+    .moveTo(x, y)
+    .lineTo(...PARAMS.guideOnePos);
+};
+
 const app = new Application();
 
 await app.init({ antialias: true, resolution: 2, autoDensity: true, background: 'white', resizeTo: window });
 
 document.body.appendChild(app.canvas);
 
+const runningTapeGraphics = new Graphics();
+app.stage.addChild(runningTapeGraphics);
+
 const tapeGraphics1 = new Graphics();
-tapeGraphics1.position.set(150, 150);
+tapeGraphics1.position.set(...PARAMS.reelOnePos);
 app.stage.addChild(tapeGraphics1);
 
 const tapeGraphics2 = new Graphics();
-tapeGraphics2.position.set(375, 150);
+tapeGraphics2.position.set(...PARAMS.reelTwoPos);
 app.stage.addChild(tapeGraphics2);
 
 const reelOne = reel();
-reelOne.position.set(150, 150);
+reelOne.position.set(...PARAMS.reelOnePos);
 app.stage.addChild(reelOne);
 const reelTwo = reel();
-reelTwo.position.set(375, 150);
+reelTwo.position.set(...PARAMS.reelTwoPos);
 app.stage.addChild(reelTwo);
 
-const totalLength = 2000000;
+const totalLength = PARAMS.tapeTotalLength;
 let currentLength = 0;
 
 app.ticker.add(time => {
   const dt = time.deltaTime;
-  const dx = 25 * dt;
+  const dx = PARAMS.tapeSpeed * dt;
   currentLength += dx;
   reelOne.rotation += dx / (2 * Math.PI * tapeRadius(totalLength - currentLength));
   reelTwo.rotation += dx / (2 * Math.PI * tapeRadius(currentLength));
   tape(tapeGraphics1.clear(), totalLength - currentLength);
   tape(tapeGraphics2.clear(), currentLength);
-  
+  runningTapeGraphics.clear().path(runningTape(totalLength - currentLength)).stroke({width: 2, color: 'black'});
 });
