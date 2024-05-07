3dc508f65b65c20b290abea60618b6b7c8c66f18
Jessie Grosen <jmgrosen@gmail.com>
2024-05-07 16:05:21 -0400

    add tape, with speed changing based on tape

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { Application, Graphics, GraphicsPath } from './pixi.mjs';
 
 const reel = () => {
-  const arcSize = Math.PI / 4;
+  const arcSize = Math.PI / 6;
   const sixth = Math.PI / 6;
   const cutPath = new GraphicsPath()
     .moveTo(0, 0)
@@ -15,10 +15,23 @@ const reel = () => {
     .closePath();
   return new Graphics()
     .circle(0, 0, 100)
-    .fill('black')
+    .fill('grey')
     .path(cutPath)
     .cut()
     .circle(0, 0, 35)
+    .fill('grey');
+};
+
+const innerRadius = 35;
+const tapeThickness = 0.01;
+
+const tapeRadius = (tapeLength) => {
+  return Math.sqrt(innerRadius * innerRadius + tapeThickness * tapeLength / Math.PI);
+};
+
+const tape = (g, length) => {
+  return g
+    .circle(0, 0, tapeRadius(length))
     .fill('black');
 };
 
@@ -28,6 +41,14 @@ await app.init({ antialias: true, resolution: 2, autoDensity: true, background: 
 
 document.body.appendChild(app.canvas);
 
+const tapeGraphics1 = new Graphics();
+tapeGraphics1.position.set(150, 150);
+app.stage.addChild(tapeGraphics1);
+
+const tapeGraphics2 = new Graphics();
+tapeGraphics2.position.set(375, 150);
+app.stage.addChild(tapeGraphics2);
+
 const reelOne = reel();
 reelOne.position.set(150, 150);
 app.stage.addChild(reelOne);
@@ -35,9 +56,16 @@ const reelTwo = reel();
 reelTwo.position.set(375, 150);
 app.stage.addChild(reelTwo);
 
+const totalLength = 2000000;
+let currentLength = 0;
+
 app.ticker.add(time => {
   const dt = time.deltaTime;
-  const dtheta = dt * 0.01;
-  reelOne.rotation += dtheta;
-  reelTwo.rotation += dtheta;
+  const dx = 25 * dt;
+  currentLength += dx;
+  reelOne.rotation += dx / (2 * Math.PI * tapeRadius(totalLength - currentLength));
+  reelTwo.rotation += dx / (2 * Math.PI * tapeRadius(currentLength));
+  tape(tapeGraphics1.clear(), totalLength - currentLength);
+  tape(tapeGraphics2.clear(), currentLength);
+  
 });
